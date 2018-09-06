00469cee4541f88ce937b7f2599efbebfddb8bb8
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-09-06 09:58:43 -0700

    Agregar archivos de proyecto.

---
+++ ExamenPrimerParcial [Imágen]/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ExamenPrimerParcial__Imágen_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cannyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.histogramBox1 = new Emgu.CV.UI.HistogramBox();
+            this.histogramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.cannyToolStripMenuItem,
+            this.histogramaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(990, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click_1);
+            // 
+            // cannyToolStripMenuItem
+            // 
+            this.cannyToolStripMenuItem.Name = "cannyToolStripMenuItem";
+            this.cannyToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.cannyToolStripMenuItem.Text = "Canny";
+            this.cannyToolStripMenuItem.Click += new System.EventHandler(this.cannyToolStripMenuItem_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.imageBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.imageBox1.Enabled = false;
+            this.imageBox1.Location = new System.Drawing.Point(0, 24);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(651, 486);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // histogramBox1
+            // 
+            this.histogramBox1.Location = new System.Drawing.Point(669, 246);
+            this.histogramBox1.Name = "histogramBox1";
+            this.histogramBox1.Size = new System.Drawing.Size(309, 252);
+            this.histogramBox1.TabIndex = 3;
+            this.histogramBox1.Load += new System.EventHandler(this.histogramBox1_Load);
+            // 
+            // histogramaToolStripMenuItem
+            // 
+            this.histogramaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.redToolStripMenuItem});
+            this.histogramaToolStripMenuItem.Name = "histogramaToolStripMenuItem";
+            this.histogramaToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.histogramaToolStripMenuItem.Text = "Histograma";
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 510);
+            this.Controls.Add(this.histogramBox1);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cannyToolStripMenuItem;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.HistogramBox histogramBox1;
+        private System.Windows.Forms.ToolStripMenuItem histogramaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+    }
+}
+
---
+++ ExamenPrimerParcial [Imágen]/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ExamenPrimerParcial__Imágen_
+{
+    public partial class Form1 : Form
+    {
+
+        Image<Bgr, byte> imgInput;
+        Image<Gray, byte> _GrayImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                imgInput = new Image<Bgr, byte>(ofd.FileName);
+                imageBox1.Image = imgInput;
+            }
+
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Salir?", "System Message", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                this.Close();
+
+
+            }
+        }
+
+        private void cannyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> imgCanny = new Image<Gray, byte>(imgInput.Width, imgInput.Height, new Gray());
+            imgCanny = imgInput.Canny(50, 50);
+            imageBox1.Image = imgCanny;
+        }
+
+        private void histogramBox1_Load(object sender, EventArgs e)
+        {
+            DenseHistogram hist = new DenseHistogram(256, new RangeF(0, 255));
+            hist.Calculate(new Image<Gray, byte>[] { imgInput[0] }, false, null);
+            Mat m = new Mat();
+            hist.CopyTo(m);
+            histogramBox1.AddHistogram("Histograma de color rojo", Color.Red, m, 256, new float[] { 0, 255 });
+            histogramBox1.Refresh();
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DenseHistogram hist = new DenseHistogram(256, new RangeF(0, 255));
+            hist.Calculate(new Image<Gray, byte>[] { imgInput[2] }, false, null);
+            Mat m = new Mat();
+            hist.CopyTo(m);
+            histogramBox1.AddHistogram("Histograma de color Rojo", Color.Red, m, 256, new float[] { 0, 255 });
+            histogramBox1.Refresh();
+        }
+
+        private void salirToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Salir?", "System Message", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                this.Close();
+
+
+            }
+        }
+    }
+}
